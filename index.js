92417e665dfee38df18c6902d8b7257607844ff0
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2022-11-24 18:05:55 +0600

    specific category laptop api create

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// const jwt = require("jsonwebtoken");
-// require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,29 +11,44 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uhbaknf.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uhbaknf.mongodb.net/?retryWrites=true&w=majority`;
 
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 // connect with database
-// const dbConnect = async () => {
-//   try {
-//     await client.connect();
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-// dbConnect();
+const dbConnect = async () => {
+  try {
+    await client.connect();
+  } catch (error) {
+    console.error(error);
+  }
+};
+dbConnect();
+
+const brandCollection = client.db("usedLaptop").collection("category");
+
+app.get("/category", async (req, res) => {
+  const query = {};
+  const category = await brandCollection.find(query).toArray();
+  res.send(category);
+});
+
+app.get("/category/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { brand: { $in: [id] } };
+  const category = await brandCollection.find(query).toArray();
+  res.send(category);
+});
 
 // root api
 app.get("/", (req, res) => {
-  res.send("Demo Server is Running");
+  res.send("Used Laptop Server is Running");
 });
 
 app.listen(port, () => {
-  console.log(`Demo server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
